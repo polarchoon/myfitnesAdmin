a949549ae578f0f5bfb830ee67b09b7ebbd6141f
polarchoon <waichoon6601@hotmail.com>
2018-04-21 12:25:04 +0800

    admin

--- src/pages/add-gallery/add-gallery.ts
+++ src/pages/add-gallery/add-gallery.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { of } from "rxjs/observable/of";
 import { PhotoProvider } from '../../providers/photo/photo';
 import { Camera } from '@ionic-native/camera';
 /**
